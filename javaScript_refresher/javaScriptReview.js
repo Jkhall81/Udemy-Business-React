9fdb9ec562a4cc119ed3d7ec0dab50280b084cb4
Jkhall81 <jason.kei.hall@gmail.com>
2023-11-02 18:06:04 -0700

    ternary operator, and and and or operator short circuiting.  never really learned this before, ternary operator was review.

--- javaScript_refresher/javaScriptReview.js
+++ javaScript_refresher/javaScriptReview.js
@@ -2,7 +2,7 @@ const data = [
   {
     id: 1,
     title: "The lord of the Rings",
-    puglicationDate: "1954-07-29",
+    publicationDate: "1954-07-29",
     author: "J. R. R. Tolkien",
     genres: [
       "fantasy",
@@ -32,8 +32,8 @@ const data = [
   },
   {
     id: 2,
-    title: "The lord of the Rings",
-    puglicationDate: "1954-07-29",
+    title: "Junkie Roomate",
+    publicationDate: "1954-07-29",
     author: "J. R. R. Tolkien",
     genres: [
       "fantasy",
@@ -64,7 +64,7 @@ const data = [
   {
     id: 3,
     title: "The lord of the Rings",
-    puglicationDate: "1954-07-29",
+    publicationDate: "1954-07-29",
     author: "J. R. R. Tolkien",
     genres: [
       "fantasy",
@@ -104,4 +104,44 @@ function getBook(id) {
 
 // destructuring
 
-const books = getBooks();
+const book = getBook(1);
+// const title = book.title;
+// title;
+
+const { title, author, genres, publicationDate, pages } = book;
+console.log(title, author, genres, publicationDate);
+
+// console.log(primaryGenre, secondaryGenre);
+
+// rest and spread operator
+const [primaryGenre, secondaryGenre, ...otherGenres] = genres;
+console.log(primaryGenre, secondaryGenre, otherGenres);
+
+const newGenres = [...genres, "epic fantasy"];
+console.log(newGenres);
+
+const updatedBook = {
+  ...book,
+  moviePublicationDate: "2001-12-19",
+  pages: 1210,
+};
+console.log(updatedBook);
+
+// template literals `${}`
+
+const summary = `${title}, a ${pages}--page long book, was written by ${author}`;
+summary;
+
+let test = pages > 1000 ? "over a thousand" : "less than one thousand";
+console.log(test);
+
+// everybody loves arrow functions () => {} quick and dirty
+
+let todaysDate = "11-02-2023";
+
+// function getYear(str) {
+//   return str.split("-")[2];
+// }
+
+let dateFunc = (str) => str.split("-")[2];
+console.log(dateFunc(todaysDate));
